69027f67264f9c7136c02525ec94cfdeb71a6016
Andrii Mutylo <amutylo@webcodin.com>
2019-11-26 17:01:10 +0200

    Added EventSubscriber to intercept the password and hash it.

---
+++ src/EventSubscriber/PasswordHashSubscriber.php
@@ -0,0 +1,52 @@
+<?php
+
+
+namespace App\EventSubscriber;
+
+use ApiPlatform\Core\EventListener\EventPriorities;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+/**
+ * Class PasswordHashSubscriber
+ * Subscribing to the password hashing event
+ * Intercept password and hash it before it save to the DB.
+ * @package App\EventSubscriber
+ */
+class PasswordHashSubscriber  implements EventSubscriberInterface
+{
+
+  /**
+   * @var UserPasswordEncoderInterface
+   */
+  private $passwordEncoder;
+
+  public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+  {
+       $this->passwordEncoder = $passwordEncoder;
+  }
+
+  public static function getSubscribedEvents() {
+    return [
+          KernelEvents::VIEW => ['hashPassword', EventPriorities::PRE_WRITE]
+    ];
+  }
+
+  public function hashPassword(GetResponseForControllerResultEvent $event)
+  {
+    $user = $event->getControllerResult();
+    $method = $event->getRequest()->getMethod();
+    if (!$user instanceof User || Request::METHOD_POST !== $method) {
+      return;
+    }
+
+    //It is an User, we need to hash password here.
+    $user->setPassword(
+      $this->passwordEncoder->encodePassword($user, $user->getPassword())
+    );
+  }
+}
