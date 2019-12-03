7861bcde51f3f265e7a4d4f2fd558a283a73d750
Andrii Mutylo <amutylo@webcodin.com>
2019-12-03 13:04:00 +0200

    show user profile fields (email and role) only to the owner

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -100,7 +100,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"post", "put", "get-admin"})
+     * @Groups({"post", "put", "get-admin", "get-owner"})
      * @Assert\NotBlank()
      * @Assert\Email()
      */
@@ -122,7 +122,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="simple_array", length=200)
-     * @Groups({"get-admin"})
+     * @Groups({"get-admin", "get-owner"})
      */
     private $roles;
 
